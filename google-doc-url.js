4021b2816cc38f53ae63e22330ee312ba614366b
Silvio Cesar <47402133+scs999@users.noreply.github.com>
2023-12-27 18:23:23 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1G1H7zEI8HHBL16Tw0bkTWrKUnBnRe3k2gcmn7xupTBM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1iqRF8uDzxp1AOgN3hH722BLmjmHYqZ_C/edit#gid=143938465';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBp2pdK3gE1KGZc_XxDdlQEVC3nUpAyuOM';
